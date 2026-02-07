1695e785f1c11dd302e27c33077a2ff0dbdf66e3
Akash kumar <akashgkr12@gmail.com>
2026-02-08 02:01:51 +0530

    fix error

--- server/server.js
+++ server/server.js
@@ -19,23 +19,22 @@ await connectDB();
 await connectCloudinary();
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://lms-website-using-mern-stack.vercel.app"
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://lms-website-using-mern-stack.vercel.app",
+      "https://lms-website-using-mern-stack-8yuhy0j3p-akashs-projects-f5c26188.vercel.app"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// REQUIRED for Vercel
+app.options("*", cors());
+
 
 
 
